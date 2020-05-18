14b68151031826cec992e3155c823a95278bf2db
Laura Kovačić <laurakciic@gmail.com>
2020-05-18 14:29:40 +0200

    Kreirane tablice i vanjski kljucevi u bazi izostanciferit

--- izostanciferit.sql
+++ izostanciferit.sql
@@ -2,3 +2,29 @@
 drop database if exists izostanciferit;
 create database izostanciferit;
 use izostanciferit;
+
+create table student(
+	sifra int not null primary key auto_increment,
+	ime varchar(50),
+	prezime varchar(50),
+	smjer varchar(50),
+	godina varchar(10),
+	jmbag char(10));
+
+create table kolegij(
+	sifra int not null primary key auto_increment,
+	naziv varchar(50) not null,
+	semestar int,
+	satipredavanja varchar(50),
+	sativjezbi varchar(50),
+	brojects int);
+
+create table izostanak(
+	sifra int not null primary key auto_increment,
+	kolegij int not null,
+	student int not null,
+	brojsati int,
+	datum datetime);
+
+alter table izostanak add foreign key (kolegij) references kolegij(sifra);
+alter table izostanak add foreign key (student) references student(sifra);
