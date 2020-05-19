3370730e831bc0f49316a45379e37dd1595127ca
Laura Kovačić <laurakciic@gmail.com>
2020-05-19 18:15:39 +0200

    Update era dijagrama i baze izostanciferit

--- izostanciferit.sql
+++ izostanciferit.sql
@@ -3,12 +3,20 @@ drop database if exists izostanciferit;
 create database izostanciferit;
 use izostanciferit;
 
+create table izostanak(
+	sifra int not null primary key auto_increment,
+	kolegij int not null,
+	student int not null,
+	brsati int,
+	datum datetime);
+
 create table student(
 	sifra int not null primary key auto_increment,
-	ime varchar(50),
-	prezime varchar(50),
-	smjer varchar(50),
-	godina varchar(10),
+	ime varchar(50) not null,
+	prezime varchar(50) not null,
+	smjer int not null,
+	grupa int,
+	godina varchar(10) not null,
 	jmbag char(10));
 
 create table kolegij(
@@ -17,15 +25,27 @@ create table kolegij(
 	semestar int,
 	satipredavanja varchar(50),
 	sativjezbi varchar(50),
-	brojects int,
+	brojects int not null,
 	nositelj varchar(50));
 
-create table izostanak(
+create table grupa(
 	sifra int not null primary key auto_increment,
-	kolegij int not null,
-	student int not null,
-	brojsati int,
-	datum datetime);
+	naziv varchar (50) not null,
+	smjer int,
+	kolegij int, 
+	student int);
+
+create table smjer(
+	sifra int not null primary key auto_increment,
+	naziv varchar(50));
 
 alter table izostanak add foreign key (kolegij) references kolegij(sifra);
 alter table izostanak add foreign key (student) references student(sifra);
+
+alter table student add foreign key (smjer) references smjer(sifra);
+
+alter table grupa add foreign key (smjer) references smjer(sifra);
+alter table grupa add foreign key (kolegij) references kolegij(sifra);
+
+alter table student add foreign key (grupa) references grupa(sifra);
+
