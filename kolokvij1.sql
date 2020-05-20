736a4d5c674f1c7097da991eff74b91b9ff03863
Laura Kovačić <laurakciic@gmail.com>
2020-05-20 21:06:48 +0200

    Update kolokvij1 do petog zadatka

--- kolokvij1.sql
+++ kolokvij1.sql
@@ -72,6 +72,51 @@ alter table cura add foreign key (mladic) references mladic(sifra);
 alter table ostavljena_snasa add foreign key (snasa) references snasa(sifra);
 alter table punac add foreign key (cura) references cura(sifra);
 
+describe cura;
+insert into cura(jmbag, prstena, bojakose) values
+(1012987343, 2, 'smeđa'),
+(1237563748, 1, 'crna'),
+(1284859203, 1, 'plava');
+select * from cura;
+
+describe ostavljena;
+insert into ostavljena(suknja, bojakose, carape) values
+('pencil', 'smeđa', 'crne stopalice'),
+('dugacka', 'crna', 'crne'),
+('dugacka', 'plava', 'bijele');
+
+describe snasa;
+insert into snasa(drugiputa, carape) values
+('2019-5-5', 'crne stopalice'),
+('2019-4-4', 'crne'),
+('2019-3-3', 'bijele');
+
+describe ostavljena_snasa;
+insert into ostavljena_snasa(ostavljena, snasa) values
+(1, 1),
+(2, 2),
+(3,3);
+
+describe mladic;
+insert into mladic(prstena, maraka, narukvica, ostavljena) values
+(2, 1000, 2, 1),
+(1, 500, 3, 2),
+(3, 100, 1, 3);
+
+update sestra set hlace = 'Osijek';
+
+delete from punac where vesta != 'AB';
+
+describe mladic;
+select suknja from mladic 
+where maraka != ('7' || '11' || '18' || '25' || '40'); 
+
+
+
+
+
+
+
 
 
 
