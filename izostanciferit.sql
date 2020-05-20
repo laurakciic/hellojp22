b1a415a829955c886fa123b4345b21ec6fdf22fa
Laura Kovačić <laurakciic@gmail.com>
2020-05-20 19:56:50 +0200

    Update era izostanciferit

--- izostanciferit.sql
+++ izostanciferit.sql
@@ -5,51 +5,53 @@ use izostanciferit;
 
 create table izostanak(
 	sifra int not null primary key auto_increment,
-	kolegij int not null,
 	student int not null,
-	brsati int,
+	grupa int,
 	datum datetime not null);
 
 create table student(
 	sifra int not null primary key auto_increment,
 	ime varchar(50) not null,
 	prezime varchar(50) not null,
-	smjer int not null,
-	grupa int,
-	godina varchar(10),
 	jmbag char(10));
 
 create table kolegij(
 	sifra int not null primary key auto_increment,
 	naziv varchar(50) not null,
+	smjer int,
 	semestar int not null,
 	satipredavanja varchar(50),
 	sativjezbi varchar(50),
+	dozvoljenoizostanaka int,
 	brojects int not null,
 	nositelj varchar(50));
 
 create table grupa(
 	sifra int not null primary key auto_increment,
 	naziv varchar (50) not null,
-	smjer int not null,
-	kolegij int not null, 
-	student int);
+	kolegij int not null);
 
 create table smjer(
 	sifra int not null primary key auto_increment,
 	naziv varchar(50) not null);
 
-alter table izostanak add foreign key (kolegij) references kolegij(sifra);
+create table clan(
+	sifra int not null primary key auto_increment,
+	student int,
+	grupa int
+	);
+
 alter table izostanak add foreign key (student) references student(sifra);
+alter table izostanak add foreign key (grupa) references grupa(sifra);
 
-alter table student add foreign key (smjer) references smjer(sifra);
-
-alter table grupa add foreign key (smjer) references smjer(sifra);
 alter table grupa add foreign key (kolegij) references kolegij(sifra);
 
-alter table student add foreign key (grupa) references grupa(sifra);
+alter table clan add foreign key (student) references student(sifra);
+alter table clan add foreign key (grupa) references grupa(sifra);
 
+alter table kolegij add foreign key (smjer) references smjer(sifra);
 # smjer, kolegij, grupa, student, izostanak
+
 show tables;
 describe smjer;
 insert into smjer(naziv) values
